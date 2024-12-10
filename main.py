844335b118e6933502f02e9d7829aa93dd190db0
brandonpowell135 <brandonpowell135@gmail.com>
2024-12-09 20:30:00 -0500

    no portfolio

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 def main():
     #Log in to Robinhood
-    if not login_to_robinhood():
-        print("Failed to log in to Robinhood.")
-        return
+    #if not login_to_robinhood():
+        #print("Failed to log in to Robinhood.")
+        #return
 
     # Set up bot commands
     setup_stock_alerts(bot, CHANNEL_ID)
     general_commands(bot)
-    portfolio_managment(bot)
+    #portfolio_managment(bot)
     backtest(bot)
 
     # Run bot
